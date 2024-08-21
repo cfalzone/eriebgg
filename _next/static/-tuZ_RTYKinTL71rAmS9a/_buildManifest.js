96fac744f25a03625f6517e400569c1517eaeea6
cfalzone <cfalzone@users.noreply.github.com>
2024-08-21 01:06:56 +0000

    Deploying to gh-pages from @ cfalzone/eriebgg@a9fb50bb34f0780532cce8fef95534ed0e3cef29 🚀

---
+++ _next/static/-tuZ_RTYKinTL71rAmS9a/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/876-5598ec3c5edc219a.js","static/chunks/pages/index-6fe2dbc71e3eafda.js"],"/_error":["static/chunks/pages/_error-d11cb7facb2c59a4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
